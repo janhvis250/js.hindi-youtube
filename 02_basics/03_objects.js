1afae513f1942ea3cb60733d057b2c89b48734b5
janhvi singh <singhjanhvi2006@gmail.com>
2026-07-29 20:13:37 +0000

    Add JavaScript basics files

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -7,12 +7,12 @@ const mySym = Symbol("key1")
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
+    name: "janhvi",
+    "full name": "janhvi singh",
     [mySym]: "mykey1",
     age: 18,
     location: "Jaipur",
-    email: "hitesh@google.com",
+    email: "janhvi@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
@@ -22,9 +22,9 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
+JsUser.email = "janhvi@chatgpt.com"
 // Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "janhvi@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
